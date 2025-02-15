cd9577c5efb7b17c79a1f0bb5f0d1012ce4178da
codermahbub20 <mkrefat5@gmail.com>
2025-02-15 19:34:59 +0600

    Products api created

---
+++ front-end/src/redux/features/product/productApi.ts
@@ -0,0 +1,60 @@
+import { baseApi } from "../../api/baseApi";
+
+const productApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: (query) => ({
+        url: "/products",
+        method: "GET",
+        params: query,
+      }),
+      //   providesTags: [tagTypes.product],
+    }),
+    getSingleProduct: builder.query({
+      query: (id: string) => ({
+        url: `/products/${id}`,
+        method: "GET",
+      }),
+      //   providesTags: [tagTypes.product],
+    }),
+    createProduct: builder.mutation({
+      query: (body) => ({
+        url: "/products",
+        method: "POST",
+        body,
+      }),
+      //   invalidatesTags: [tagTypes.product],
+    }),
+    editProduct: builder.mutation({
+      query: ({ id, body }) => ({
+        url: `/products/${id}`,
+        method: "PATCH",
+        body,
+      }),
+      //   invalidatesTags: [tagTypes.product],
+    }),
+    deleteProduct: builder.mutation({
+      query: (id: string) => ({
+        url: `/products/${id}`,
+        method: "DELETE",
+      }),
+      //   invalidatesTags: [tagTypes.product],
+    }),
+    getPriceRange: builder.query({
+      query: () => ({
+        url: "/products/price-range",
+        method: "GET",
+      }),
+      //   providesTags: [tagTypes.product],
+    }),
+  }),
+});
+
+export const {
+  useGetProductsQuery,
+  useGetSingleProductQuery,
+  useCreateProductMutation,
+  useEditProductMutation,
+  useDeleteProductMutation,
+  useGetPriceRangeQuery,
+} = productApi;
---
+++ front-end/src/types/tagTypes.ts
@@ -0,0 +1,13 @@
+export const tagTypes = {
+  user: "user",
+  profile: "profile",
+  product: "product",
+  order: "order",
+};
+
+export const tagTypesList = [
+  tagTypes.profile,
+  tagTypes.product,
+  tagTypes.order,
+  tagTypes.user,
+];
